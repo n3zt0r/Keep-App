4d40b57fe8b2d86dfc92f984309d5fd968481a85
Nestor Lopez <n3zt0r77@gmail.com>
2023-10-03 17:22:27 -0600

    Adding Material UI component and styling the app

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,46 +1,60 @@
 import React, { useState } from "react";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import Zoom from "@mui/material/Zoom";
 
 export const CreateArea = (props) => {
+  const [isExpand, setExpand] = useState(false);
   const [content, setContent] = useState({
     title: "",
     content: "",
   });
 
-  const addContent = (event) => {
+  const handleChange = (event) => {
     const { value, name } = event.target;
     setContent((prevValue) => {
       return { ...prevValue, [name]: value };
     });
   };
 
+  const handleClick = (event) => {
+    props.onAdd(content);
+    setContent({
+      title: "",
+      content: "",
+    });
+
+    event.preventDefault();
+  };
+
+  const expand = () => {
+    setExpand(true);
+  };
+
   return (
     <div>
-      <form>
-        <input
-          onChange={addContent}
-          name="title"
-          placeholder="Title"
-          value={content.title}
-        />
+      <form className="create-note">
+        {isExpand && (
+          <input
+            onChange={handleChange}
+            name="title"
+            placeholder="Title"
+            value={content.title}
+          />
+        )}
         <textarea
-          onChange={addContent}
+          onClick={expand}
+          onChange={handleChange}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpand ? 3 : 1}
           value={content.content}
         />
-        <button
-          onClick={(event) => {
-            event.preventDefault();
-            props.onAdd(content);
-            setContent({
-              title: "",
-              content: "",
-            });
-          }}
-        >
-          Add
-        </button>
+        <Zoom in={isExpand}>
+          <Fab onClick={handleClick}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import HighlightIcon from "@mui/icons-material/Highlight";
 
 export const Header = () => {
-    return (
-        <header>
-            <h1>Keeper App</h1>
-        </header>
-    )
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        Keeper App
+      </h1>
+    </header>
+  );
 };
